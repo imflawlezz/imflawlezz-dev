46b6c2d54d55cc65aefe127ca42ef2b743fb0395
imflawlezz <egorart6@gmail.com>
2025-07-02 23:11:26 +0200

    feat: add test InDev.tsx component, adjust page.tsx layout

---
+++ src/components/sections/InDev.tsx
@@ -0,0 +1,8 @@
+export const InDev = () => {
+    return (
+        <section className="w-full h-full flex flex-col items-center justify-center">
+            <h1 className="text-4xl leading-tight">Site in development</h1>
+            <p className="text-lg uppercase">If you are seeing this, then everything works fine</p>
+        </section>
+    )
+}
