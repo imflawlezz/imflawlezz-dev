b259e8ba5aab5a0b5ca80dec6a3815fd7fb8bbc2
imflawlezz <egorart6@gmail.com>
2025-07-02 23:18:24 +0200

    style: adjust section appearance

--- src/components/sections/InDev.tsx
+++ src/components/sections/InDev.tsx
@@ -1,8 +1,15 @@
 export const InDev = () => {
     return (
-        <section className="w-full h-full flex flex-col items-center justify-center">
-            <h1 className="text-4xl leading-tight">Site in development</h1>
-            <p className="text-lg uppercase">If you are seeing this, then everything works fine</p>
+        <section className="w-screen h-screen flex flex-col gap-4 text-center items-center justify-center">
+            <h1 className="text-4xl leading-tight uppercase">Site in development</h1>
+            <p className="text-sm">If you are seeing this, then everything works fine</p>
+            <p>GitHub: <a
+                href="https://github.com/imflawlezz"
+                className="underline hover:text-red-400 transition-all duration-200"
+            >
+                imflawlezz
+            </a>
+            </p>
         </section>
     )
 }
