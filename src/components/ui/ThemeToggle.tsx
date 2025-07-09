77825d73fb5a32d57a4dc08cc559e2745483aea0
imflawlezz <egorart6@gmail.com>
2025-07-10 00:23:46 +0200

    style: adjust icon size on smaller screens

--- src/components/ui/ThemeToggle.tsx
+++ src/components/ui/ThemeToggle.tsx
@@ -7,11 +7,11 @@ export const ThemeToggle = () => {
     const { theme, toggleTheme } = useTheme();
 
     return (
-        <button onClick={toggleTheme} className="text-muted transition duration-500">
+        <button onClick={toggleTheme} className="w-8 h-8 md:w-6 md:h-6 text-muted transition duration-500">
             {theme === 'light' ? (
-                <MoonIcon className="w-6 h-6 hover:rotate-30 transition duration-500" />
+                <MoonIcon className="hover:rotate-30 transition duration-500" />
             ) : (
-                <SunIcon className="w-6 h-6 hover:rotate-60 transition duration-500" />
+                <SunIcon className="hover:rotate-60 transition duration-500" />
             )}
         </button>
     );
