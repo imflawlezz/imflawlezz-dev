2e1a448b6037743e4e609fe526da76722a16cd18
imflawlezz <egorart6@gmail.com>
2025-07-10 01:00:15 +0200

    style: update link appearance, add hover transition

--- src/components/shared/LinkComponent.tsx
+++ src/components/shared/LinkComponent.tsx
@@ -10,8 +10,8 @@ export const LinkComponent = ({
     return (
         <Link
             href={href}
-            className={`${className} font-bold text-accent hover:text-accent-hover text-lg md:text-xl 2xl:text-2xl
-            flex flex-row items-center gap-2`}
+            className={`${className} font-bold text-accent hover:text-accent-hover text-lg md:text-xl
+            flex flex-row items-center gap-2 transition-all duration-300`}
         >
             {label}
             <ArrowDownRightIcon className="w-5 h-5 lg:stroke-3" />
