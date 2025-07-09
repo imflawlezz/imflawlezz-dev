006bf67199e3fdd451dd44a9c3a0fa12feb123e6
imflawlezz <egorart6@gmail.com>
2025-07-09 17:48:45 +0200

    feat: adjust component for new togglable themes

--- src/components/ui/ThemeToggle.tsx
+++ src/components/ui/ThemeToggle.tsx
@@ -1,11 +1,18 @@
-import {SunIcon} from "@heroicons/react/24/outline";
+'use client';
+
+import { SunIcon, MoonIcon } from '@heroicons/react/24/outline';
+import { useTheme } from '@/hooks/useTheme';
 
 export const ThemeToggle = () => {
+    const { theme, toggleTheme } = useTheme();
+
     return (
-        <button>
-            <SunIcon
-                className="w-6 h-6 hover:rotate-90 transition duration-500 cursor-pointer"
-            />
+        <button onClick={toggleTheme} className="text-muted transition duration-500">
+            {theme === 'light' ? (
+                <MoonIcon className="w-6 h-6 hover:rotate-30 transition duration-500" />
+            ) : (
+                <SunIcon className="w-6 h-6 hover:rotate-60 transition duration-500" />
+            )}
         </button>
-    )
-}
+    );
+};
