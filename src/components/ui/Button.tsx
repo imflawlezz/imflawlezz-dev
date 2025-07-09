53a437efb78205f92161d40d613863b9708dbede
imflawlezz <egorart6@gmail.com>
2025-07-09 17:47:46 +0200

    style: change primary button foreground color to white

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -13,7 +13,8 @@ export const Button = ({
             href={linkHref ? `/${linkHref}` : `#`}
             onClick={onClick}
             className={`${className} px-6 py-3 text-base rounded-xl transition duration-300 ease-in-out
-                ${isPrimary ? 'text-foreground bg-accent hover:bg-accent-hover' : 'text-accent bg-transparent hover:text-foreground hover:bg-accent-hover hover:border-accent-hover border-accent border-1'}
+                ${isPrimary ? 'text-white bg-accent hover:bg-accent-hover' : 
+                'text-accent bg-transparent hover:text-foreground hover:bg-accent-hover hover:border-accent-hover border-accent border-1'}
             `}
         >
             {label}
