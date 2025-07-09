8aad29c3dd7f4f562ed55f02ca5e29d03079da8f
imflawlezz <egorart6@gmail.com>
2025-07-10 00:50:53 +0200

    feat: update root/page.tsx layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
 import {Hero} from "@/components/pages/home/Hero";
+import {FeaturedProjects} from "@/components/pages/home/FeaturedProjects";
 
 export default function Home() {
     return (
-        <Hero/>
+        <>
+            <Hero/>
+            <FeaturedProjects/>
+        </>
     );
 }
