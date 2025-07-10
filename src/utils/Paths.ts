1e6534555508ca62efac0a4b2ba15ecca41927a6
imflawlezz <egorart6@gmail.com>
2025-07-10 19:43:54 +0200

    chore: update Paths.ts

--- src/utils/Paths.ts
+++ src/utils/Paths.ts
@@ -4,6 +4,8 @@ export const assetPaths = {
     icons: `${assetRoot}icons`,
     projects: `${assetRoot}projects`,
     sections: `${assetRoot}sections`,
+    hero: `${assetRoot}sections/hero`,
+    cta: `${assetRoot}sections/cta`,
 } as const;
 
 export type AssetCategory = keyof typeof assetPaths;
