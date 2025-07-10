78a35d788a0965a1cefee079504e6ba13c742c20
imflawlezz <egorart6@gmail.com>
2025-07-10 19:47:06 +0200

    feat: update root/page.tsx layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 import {Hero} from "@/components/pages/home/Hero";
 import {FeaturedProjects} from "@/components/pages/home/FeaturedProjects";
 import {StackOverview} from "@/components/pages/home/StackOverview";
+import {CallToAction} from "@/components/shared/CallToAction";
+import {callToActionVariants} from "@/data/cta";
 
 export default function Home() {
     return (
@@ -8,6 +10,7 @@ export default function Home() {
             <Hero/>
             <FeaturedProjects/>
             <StackOverview/>
+            <CallToAction {...callToActionVariants.home} />
         </>
     );
 }
