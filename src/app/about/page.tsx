444bbdd25d0eb42957db26ba35dfe53ec17edf90
imflawlezz <egorart6@gmail.com>
2025-07-12 12:43:47 +0200

    feat: adjust about/page.tsx layout

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,6 +1,8 @@
 import {Hero} from "@/components/shared/Hero";
-import {heroContent} from "@/data/about";
+import {heroContent, valueContent} from "@/data/about";
 import {Timeline} from "@/components/pages/about/Timeline";
+import {ImageSplitSection} from "@/components/shared/ImageSplitSection";
+import {SectionHeading} from "@/components/ui/SectionHeading";
 
 export default function About() {
     return (
@@ -11,6 +13,16 @@ export default function About() {
             imageUrl={heroContent.imageUrl}
             />
             <Timeline/>
+            <ImageSplitSection imageUrl={valueContent.imageUrl} reverse={false}>
+                <SectionHeading heading={valueContent.heading}/>
+                <div className="flex flex-col gap-4">
+                    {valueContent.paragraph.map((paragraph, index) => (
+                        <p key={index}>
+                            {paragraph}
+                        </p>
+                    ))}
+                </div>
+            </ImageSplitSection>
         </>
     );
 }
