179df74c8f68b03323fdbfde4c08a818ef9ac005
imflawlezz <egorart6@gmail.com>
2025-07-14 21:45:24 +0200

    chore: add types and sample locale files for "Home" page

---
+++ src/types/home.ts
@@ -0,0 +1,39 @@
+import {ButtonProps} from "@/types/ui";
+
+export type HeadingProps = {
+    text: string;
+    className?: string;
+    br?: boolean;
+    prefix?: string;
+}
+export type HeroProps = {
+    heading: HeadingProps[];
+    paragraph: string[];
+    buttons: ButtonProps[];
+}
+
+export type FeaturedProjectsProps = {
+    heading: string;
+    linkContent: {
+        label: string,
+        href: string,
+    }
+}
+
+export type StackOverviewProps = {
+    heading: string;
+    stack: {
+        name: string,
+        icon: string
+    }[]
+    ,
+    linkContent: {
+        label: string,
+        href: string,
+    }
+}
+
+export type ShortAboutMeProps = {
+    heading: string;
+    paragraph: string[];
+}
