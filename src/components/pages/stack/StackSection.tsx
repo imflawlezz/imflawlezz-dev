c8ef07e23b726e547c5a5b15240365ac5b35c8f0
imflawlezz <egorart6@gmail.com>
2025-07-14 13:57:21 +0200

    style: adjust background color

--- src/components/pages/stack/StackSection.tsx
+++ src/components/pages/stack/StackSection.tsx
@@ -3,7 +3,8 @@ import {ToolsGrid} from "@/components/pages/stack/ToolsGrid";
 
 export const StackSection = () => {
     return (
-        <section className="flex flex-col items-center gap-24 p-10 sm:px-20 sm:py-12 md:px-28 md:py-14 lg:px-36 lg:py-18">
+        <section className="flex flex-col items-center gap-24 bg-background-variant
+        p-10 sm:px-20 sm:py-12 md:px-28 md:py-14 lg:px-36 lg:py-18">
             <StackGrid/>
             <ToolsGrid/>
         </section>
