233c505d30e0af30300e08143e1f27c7ba955621
imflawlezz <egorart6@gmail.com>
2025-07-14 23:57:28 +0200

    refactor: remove unused data

--- src/data/layout.ts
+++ src/data/layout.ts
@@ -1,32 +1,3 @@
-import {resolveAsset} from "@/utils/resolveAsset";
-import type { NavItem } from '@/types/navigation';
-
-export const navItems: NavItem[] = [
-    { label: 'Home', href: '/' },
-    { label: 'About Me', href: '/about' },
-    { label: 'Projects', href: '/projects' },
-    { label: 'Stack', href: '/stack' },
-    { label: 'Contact', href: '/contact' },
-];
-
-export const footerLinks = [
-    {
-        label: "Telegram",
-        linkHref: "#",
-        icon: resolveAsset('icons', 'telegram.svg')
-    },
-    {
-        label: "GitHub",
-        linkHref: "#",
-        icon: resolveAsset('icons', 'github.svg')
-    },
-    {
-        label: "LinkedIn",
-        linkHref: "#",
-        icon: resolveAsset('icons', 'linkedin.svg')
-    },
-];
-
 export const projectPageNavContent = {
     home: 'Home',
     next: 'Next',
