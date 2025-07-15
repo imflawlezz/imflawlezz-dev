34886aa94b913b12884d52aba466c9d5602be03c
imflawlezz <egorart6@gmail.com>
2025-07-15 12:59:22 +0200

    chore: adjust page.tsx layout

--- src/app/[locale]/about/page.tsx
+++ src/app/[locale]/about/page.tsx
@@ -1,17 +1,21 @@
 import {Hero} from "@/components/shared/Hero";
-import {beyondContent, callToAction, heroContent, valueContent} from "@/data/about";
 import {Timeline} from "@/components/pages/about/Timeline";
 import {CallToAction} from "@/components/shared/CallToAction";
 import {ImageSplitSectionRenderer} from "@/components/shared/ImageSplitSectionRenderer";
+import {Locale} from "@/lib/i18n/config";
+import {getPageMessages} from "@/lib/i18n/getPageMessgaes";
+import {AboutMessages} from "@/types/i18n";
 
-export default function About() {
+export default async function About({ params }: { params: { locale: Locale } }) {
+    const t = (await getPageMessages(params.locale, 'about')) as AboutMessages;
+
     return (
         <>
-            <Hero {...heroContent}/>
-            <Timeline/>
-            <ImageSplitSectionRenderer content={valueContent}/>
-            <ImageSplitSectionRenderer content={beyondContent}/>
-            <CallToAction {...callToAction}/>
+            <Hero {...t.heroContent}/>
+            <Timeline {...t.journeyContent} />
+            <ImageSplitSectionRenderer {...t.valueContent} />
+            <ImageSplitSectionRenderer {...t.beyondContent} />
+            <CallToAction {...t.callToAction}/>
         </>
     );
 }
